9da3428181e78b1c29fe21fb0b7c4b5fd6fbc043
Phanthom-Mekat <jahidulhossainmekat@gmail.com>
2024-11-03 20:18:53 +0600

    f

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,5 @@
 import Banner from "../components/Home_Components/Banner";
+import Heading from "../components/Home_Components/Heading";
 
 const Home = () => {
     return (
@@ -6,6 +7,7 @@ const Home = () => {
            {/* banner */}
             <Banner/>
            {/* Heading */}
+           <Heading title="Brows Coffees by Categories" subtitle={'Choose Your Favourite coffee and enjoy coffee at home as well as at office'}/>
            {/* categories tab section */}
            {/* dynamic nested component */}
         </div>
